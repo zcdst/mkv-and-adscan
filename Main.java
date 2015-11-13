e0a367d6e3188dea17b99a91de41dfc8affdc843
Zach <zeddost@gmail.com>
2015-11-13 11:49:55 +1000

    cleanup

--- Main.java
+++ Main.java
@@ -5,71 +5,73 @@ public class Main {
 
 
 	static HashSet<Recording> recs = new HashSet<Recording>();
-	static String comskip = "\"C:\\Program Files (x86)\\Comskip\\comskip.exe\"";
-	static String mkvmerge = "\"C:\\Program Files\\MKVToolNix\\mkvmerge.exe\"";
+	static final String COMSKIP = "\"C:\\Program Files (x86)\\Comskip\\comskip.exe\"";
+	static final String MKVMERGE = "\"C:\\Program Files\\MKVToolNix\\mkvmerge.exe\"";
+	static final String DIR = "P:\\";
 
 
 	public static void main(String[] args) throws Exception {
 
 		// File[] root = new File("P:\\").listFiles();
-		File[] root = new File("P:\\").listFiles();
+		File[] root = new File(DIR).listFiles();
 
 		walk(root);
 
-
+		int count = 1;
 		for (Recording x : recs) {
-			if (!x.processed) {	// remux the file
-				System.out.println("Re-muxing: " + x.recording);
-				String tmp = x.recording + "_mux.mkv";
+			System.out.println("------Re-muxing!-------");
+			System.out.println("File " + count + " of " + recs.size());
+			System.out.println("Re-muxing: " + x.recording);
+			String tmp = x.recording + "_mux.mkv";
 
-				ProcessBuilder p = new ProcessBuilder(mkvmerge, "-o", tmp, x.recording); 	// run mkvmerge
-				p.inheritIO();
-				Process pr = p.start();
-				pr.waitFor();
+			ProcessBuilder cspb = new ProcessBuilder(MKVMERGE, "-o", tmp, x.recording); 	// run mkvmerge
+			cspb.inheritIO();
+			Process csp = cspb.start();
+			csp.waitFor();
 
-				System.out.println("---Muxing Complete!---");
+			System.out.println("------Muxing Complete!------");
 
-				File o = new File(x.recording);
-				File n = new File(tmp);
+			File o = new File(x.recording);
+			File n = new File(tmp);
 
-				if (n.length() > 0.75 * o.length()) {	// check if muxed correctly
-					System.out.println("Output seems OK, deleting " + x.recording + " !");
-				} else {
-					n.delete();
-					continue;
-				}
-
-				o.delete();	// delete original file, rename muxed mkv to original ts
-				Thread.sleep(2000);
-				n.renameTo(new File(x.recording));
-
-				Thread.sleep(2000);
-
-				ProcessBuilder pz = new ProcessBuilder(comskip, x.recording);	// run comskip
-				pz.inheritIO();
-				Process prz = pz.start();
-				prz.waitFor();
-
-				System.out.println("---Muxing Complete!---");
-
-				String m = x.recording.substring(0, x.recording.lastIndexOf('.'));	// rename vdr file
-				String d = x.recording.substring(0, x.recording.lastIndexOf('\\') + 1);
-				File comskipout = new File(m + ".vdr");
-				File marks = new File(d + "marks");
-				if (!marks.exists()) {	// don't replace marks file if exists
-					comskipout.renameTo(marks);
-				}
-
-
-
+			if (n.length() > 0.75 * o.length()) {	// check if muxed correctly
+				System.out.println("Output seems OK, deleting " + x.recording + " !");
+			} else {
+				n.delete();
+				continue;
 			}
+
+			o.delete();	// delete original file, rename muxed mkv to original ts
+			Thread.sleep(2000);
+			n.renameTo(new File(x.recording));
+
+			Thread.sleep(2000);
+
+			ProcessBuilder mkpb = new ProcessBuilder(COMSKIP, x.recording);	// run comskip
+			mkpb.inheritIO();
+			Process mkp = mkpb.start();
+			mkp.waitFor();
+
+			System.out.println("------Scanning for ads!------");
+			System.out.println("File " + count + " of " + recs.size());
+			System.out.println("Scanning: " + x.recording);
+
+			String m = x.recording.substring(0, x.recording.lastIndexOf('.'));	// rename vdr file
+			String d = x.recording.substring(0, x.recording.lastIndexOf('\\') + 1);
+			File comskipout = new File(m + ".vdr");
+			File marks = new File(d + "marks");
+			if (!marks.exists()) {	// don't replace marks file if exists
+				comskipout.renameTo(marks);
+			}
+
+			count++;
 
 		}
 
 		System.out.println("------------------");
 	
 		for (Recording x : recs) {
-			System.out.println("Processed" + x.recording);
+			System.out.println("Processed: " + x.recording);
 		}
 
 	}
--- Recording.java
+++ Recording.java
@@ -2,7 +2,6 @@ public class Recording {
 
 
 	public String recording;
-	public boolean processed;
 	public boolean locked = false;
 
 	public Recording(String r) {
