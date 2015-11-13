90df855eb4b78576f022c2c032ebe2d93f61e5d8
Zach <zeddost@gmail.com>
2015-11-13 17:31:07 +1000

    increase last-modified to 10min

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
 	static final String COMSKIP = "\"C:\\Program Files (x86)\\Comskip\\comskip.exe\"";
 	static final String MKVMERGE = "\"C:\\Program Files\\MKVToolNix\\mkvmerge.exe\"";
 	static final String DIR = "P:\\";
-	static boolean force = false;	// when true, does not exit program when a "recently modified" recording is found
+	static boolean force = false;	// default behaviour is to do nothing if a recording is active; setting this to true ignores active recordings
 
 	Set<String> ignore = new HashSet<String>(Arrays.asList("a", "b"));
 
@@ -21,60 +21,66 @@ public class Main {
 		}
 
 
-		File[] root = new File("P:\\").listFiles();
+		File[] root = new File(DIR).listFiles();
 
 		walk(root);
 
 		int count = 1;
-		for (Recording x : recs) {
-			System.out.println("------Re-muxing!-------");
-			System.out.println("File " + count + " of " + (recs.size()));
-			System.out.println("Re-muxing: " + x.recording);
-			String tmp = x.recording + "_mux.mkv";
 
-			ProcessBuilder cspb = new ProcessBuilder(MKVMERGE, "-o", tmp, x.recording); 	// run mkvmerge
-			cspb.inheritIO();
-			Process csp = cspb.start();
-			csp.waitFor();
+		if (recs.size() > 0) {
+			for (Recording x : recs) {
+				System.out.println("------Re-muxing!-------");
+				System.out.println("File " + count + " of " + (recs.size()));
+				System.out.println("Re-muxing: " + x.recording);
+				String tmp = x.recording + "_mux.mkv";
 
-			System.out.println("------Muxing Complete!------");
+				ProcessBuilder cspb = new ProcessBuilder(MKVMERGE, "-o", tmp, x.recording); 	// run mkvmerge
+				cspb.inheritIO();
+				Process csp = cspb.start();
+				csp.waitFor();
 
-			File o = new File(x.recording);
-			File n = new File(tmp);
+				System.out.println("------Muxing Complete!------");
 
-			if (n.length() > 0.5 * o.length()) {	// check if muxed correctly
-				System.out.println("Output seems OK, deleting " + x.recording + " !");
-			} else {
-				System.out.println("Output too small, deleting muxed file!");
-				n.delete();
-				continue;
+				File o = new File(x.recording);
+				File n = new File(tmp);
+
+				if (n.length() > 0.5 * o.length()) {	// check if muxed correctly
+					System.out.println("Output seems OK, deleting " + x.recording + " !");
+				} else {
+					System.out.println("Output too small, deleting muxed file!");
+					n.delete();
+					continue;
+				}
+
+				o.delete();	// delete original file, rename muxed mkv to original ts
+				Thread.sleep(2000);
+				n.renameTo(new File(x.recording));
+
+				Thread.sleep(2000);
+
+				ProcessBuilder mkpb = new ProcessBuilder(COMSKIP, x.recording);	// run comskip
+				mkpb.inheritIO();
+				Process mkp = mkpb.start();
+				mkp.waitFor();
+
+				System.out.println("------Scanning for ads!------");
+				System.out.println("File " + count + " of " + (recs.size()));
+				System.out.println("Scanning: " + x.recording);
+
+				String m = x.recording.substring(0, x.recording.lastIndexOf('.'));	// rename vdr file
+				String d = x.recording.substring(0, x.recording.lastIndexOf('\\') + 1);
+				File comskipout = new File(m + ".vdr");
+				File marks = new File(d + "marks");
+				if (!marks.exists()) {	// don't replace marks file if exists
+					comskipout.renameTo(marks);
+				}
+
+				count++;
+
 			}
-
-			o.delete();	// delete original file, rename muxed mkv to original ts
-			Thread.sleep(2000);
-			n.renameTo(new File(x.recording));
-
-			Thread.sleep(2000);
-
-			ProcessBuilder mkpb = new ProcessBuilder(COMSKIP, x.recording);	// run comskip
-			mkpb.inheritIO();
-			Process mkp = mkpb.start();
-			mkp.waitFor();
-
-			System.out.println("------Scanning for ads!------");
-			System.out.println("File " + count + " of " + (recs.size()));
-			System.out.println("Scanning: " + x.recording);
-
-			String m = x.recording.substring(0, x.recording.lastIndexOf('.'));	// rename vdr file
-			String d = x.recording.substring(0, x.recording.lastIndexOf('\\') + 1);
-			File comskipout = new File(m + ".vdr");
-			File marks = new File(d + "marks");
-			if (!marks.exists()) {	// don't replace marks file if exists
-				comskipout.renameTo(marks);
-			}
-
-			count++;
-
+		} else {
+			System.out.println("No recordings to process!");
+			System.exit(-1);
 		}
 
 		System.out.println("------------------");
@@ -107,15 +113,14 @@ public class Main {
 
 
 	public static boolean checkTime(File f) {		// true if file has not been written to in last 5 min
-		if (System.currentTimeMillis() - f.lastModified() > 300000) {
+		if (System.currentTimeMillis() - f.lastModified() > 600000) {
 			return true;
 		} else if (force) {	// ignore this recording, but continue adding others
 			return false;
 		} else {
-			// System.out.println("Recording in progress or recently completed... aborting!");
-			// System.exit(-1);
-			// return false;
-			return true;
+			System.out.println("Recording in progress (or recently completed)... aborting!");
+			System.exit(-1);
+			return false;
 		}
 	}
 
